2ff0d1307d79ce9fa3d24a49adc6a387099b184b
Rajshri Wagh <mahajan.rajshri16@gmail.com>
2022-12-26 18:59:40 +0530

    Add: [200~Ability to create a Contacts in Address

--- src/main/java/com/AddressBook/App.java
+++ src/main/java/com/AddressBook/App.java
@@ -1,9 +1,9 @@
 package com.AddressBook;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book System!");
+		Contacts con = new Contacts();
+		System.out.println(con);
+	}
 }
